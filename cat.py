ac1e6e13cf8919140986cb55f632ca36b4738b5f
Max Walder <max.walder@protonmail.com>
2024-01-25 16:19:41 +0100

    Moved modifiers of how much food/energy/excitement is gained/lost from constants into the cat itself

--- cat.py
+++ cat.py
@@ -4,39 +4,85 @@ import os
 
 
 class Cat:
-    def __init__(self, name: str, max_food=100.0, max_energy=100.0, max_excitement=100.0):
+    def __init__(self, name: str,
+                 max_food=100.0,
+                 food_gain_modifier=10.0,
+                 food_drain_modifier=100.0,
+                 max_energy=100.0,
+                 energy_gain_modifier=100.0,
+                 energy_drain_modifier=5.0,
+                 max_excitement=100.0,
+                 excitement_gain_modifier=0.5,
+                 excitement_drain_modifier=100.0,
+                 ):
+        """
+        :param name: Name of the cat. Purely cosmetic
+        :param max_food: Maximum value of the food meter
+        :param food_gain_modifier: Food gained per commit
+        :param food_drain_modifier: Food lost per day
+        :param max_energy: Maximum value of the energy meter
+        :param energy_gain_modifier: Energy gained per day
+        :param energy_drain_modifier: Energy lost per file touched
+        :param max_excitement: Maximum value of the excitement meter
+        :param excitement_gain_modifier: Excitement gained per line added
+        :param excitement_drain_modifier: Excitement lost per day
+        """
         self.name = name
-        self.max_food = max_food
-        self.max_energy = max_energy
-        self.max_excitement = max_excitement
 
+        self.max_food = max_food
+        self.food_gain_modifier = food_gain_modifier
+        self.food_drain_modifier = food_drain_modifier
         self.food = self.max_food
+
+        self.max_energy = max_energy
+        self.energy_gain_modifier = energy_gain_modifier
+        self.energy_drain_modifier = energy_drain_modifier
         self.energy = self.max_energy
+
+        self.max_excitement = max_excitement
+        self.excitement_gain_modifier = excitement_gain_modifier
+        self.excitement_drain_modifier = excitement_drain_modifier
         self.excitement = self.max_excitement
 
         self.last_update = datetime.now(timezone.utc)
 
     def pet(self):
+        """A manual way to increase cat excitement. Besides, who doesn't like petting cats?"""
         # TODO
         pass
 
-    def feed(self, amount: float):
-        self.food = min(self.food + amount, self.max_food)
+    def nap(self):
+        """A manual way to increase cat energy"""
+        # TODO
+        pass
 
-    def hunger(self, amount: float):
-        self.food = max(0.0, self.food - amount)
+    def update_by_time_passed(self, days: float):
+        """
+        Updates the cat needs by the given amount of days.
+        :param days: a float representing the amount of days that passed since the last update
+        :return: None
+        """
+        self.hunger(days)
+        self.bore(days * self.excitement_drain_modifier)
+        self.recharge(days * self.energy_gain_modifier)
+
+    def feed(self, amount: float):
+        self.food = min(self.food + amount * self.food_gain_modifier, self.max_food)
+
+    def hunger(self, days: float):
+        self.food = max(0.0, self.food - days * self.food_drain_modifier)
 
     def recharge(self, amount: float):
-        self.energy = min(self.energy + amount, self.max_energy)
+        self.energy = min(self.energy + amount * self.energy_gain_modifier, self.max_energy)
 
-    def exhaust(self, amount: float):
-        self.energy = max(0.0, self.energy - amount)
+    def exhaust(self, days: float):
+        self.energy = max(0.0, self.energy - days * self.energy_drain_modifier)
 
     def excite(self, amount: float):
-        self.excitement = min(self.excitement + amount, self.max_excitement)
+        self.excitement = min(self.excitement + amount + self.excitement_gain_modifier, self.max_excitement)
 
-    def bore(self, amount: float):
-        self.excitement = max(0.0, self.excitement - amount)
+    def bore(self, days: float):
+        self.excitement = max(0.0, self.excitement - days * self.excitement_drain_modifier)
 
     @staticmethod
     def load(name: str) -> 'Cat':
@@ -48,13 +94,22 @@ class Cat:
                 data = json.load(infile)
                 cat = Cat(name)
                 cat.name = data['name']
-                cat.max_food = data['max_food']
-                cat.max_energy = data['max_energy']
-                cat.max_excitement = data['max_excitement']
 
+                cat.max_food = data['max_food']
+                cat.food_gain_modifier = data['food_gain_modifier']
+                cat.food_drain_modifier = data['food_drain_modifier']
                 cat.food = data['food']
+
+                cat.max_energy = data['max_energy']
+                cat.energy_gain_modifier = data['energy_gain_modifier']
+                cat.energy_drain_modifier = data['energy_drain_modifier']
                 cat.energy = data['energy']
+
+                cat.max_excitement = data['max_excitement']
+                cat.excitement_gain_modifier = data['excitement_gain_modifier']
+                cat.excitement_drain_modifier = data['excitement_drain_modifier']
                 cat.excitement = data['excitement']
+
                 cat.last_update = datetime.fromtimestamp(data['last_update'], timezone.utc)
                 return cat
         except FileNotFoundError:
@@ -67,12 +122,22 @@ class Cat:
         with open(os.path.join('.gittycat', 'cats', f'{self.name}.json'), 'w') as outfile:
             data = {
                 'name': self.name,
+
                 'max_food': self.max_food,
-                'max_energy': self.max_energy,
-                'max_excitement': self.max_excitement,
+                'food_gain_modifier': self.food_gain_modifier,
+                'food_drain_modifier': self.food_drain_modifier,
                 'food': self.food,
+
+                'max_energy': self.max_energy,
+                'energy_gain_modifier': self.energy_gain_modifier,
+                'energy_drain_modifier': self.energy_drain_modifier,
                 'energy': self.energy,
+
+                'max_excitement': self.max_excitement,
+                'excitement_gain_modifier': self.excitement_gain_modifier,
+                'excitement_drain_modifier': self.excitement_drain_modifier,
                 'excitement': self.excitement,
+
                 'last_update': self.last_update.timestamp()
             }
             json.dump(data, outfile, indent=2)
--- gittycat.py
+++ gittycat.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os.path
 import shutil
 from datetime import datetime, timezone
@@ -8,13 +10,6 @@ import argh
 from argh import arg
 
 from cat import Cat
-
-FOOD_PER_COMMIT = 10.0
-EXCITEMENT_PER_LINE = 0.5
-
-HUNGER_PER_DAY = 100.0
-ENERGY_PER_DAY = 100.0
-BOREDOM_PER_DAY = 100.0
 
 
 def _commit_changes(message: str, author='Gittycat <gittycat@example.com>'):
@@ -48,6 +43,7 @@ def adopt(name: str, **kwargs):
 
 @arg('name', type=str, help='Name of your new cat')
 def status(name: str):
+    # TODO: I'm not happy with the name of this command. a status command should not change anything
     """
     Gives back the current state of your cat.
     Is also used to process any new changed to the repository that were made since the last use of Gittycat
@@ -57,12 +53,11 @@ def status(name: str):
 
     # Get days since last update, including partial days
     days_since_last_update = (datetime.now(timezone.utc) - cat.last_update).total_seconds() / 86400.0
-    # Adjust cat needs based on time passed
-    cat.hunger(days_since_last_update * HUNGER_PER_DAY)
-    cat.bore(days_since_last_update * BOREDOM_PER_DAY)
-    cat.recharge(days_since_last_update * ENERGY_PER_DAY)
+    # Update cat state based on time passed
+    cat.update_by_time_passed(days_since_last_update)
 
     # Iterate through all commits since last update
+    print('Processing commits since last update...')
     for commit in repo.iter_commits('HEAD'):
         # Terminate once we reach a commit older than the last update
         if commit.committed_datetime < cat.last_update:
@@ -71,17 +66,20 @@ def status(name: str):
         if 'Gittycat' in commit.author.name:
             continue
         # Process commit
-        print(f'Processing commit {commit.hexsha} by {commit.author.name} ({commit.message.strip()})')
+        print(f'  Processing commit {commit.hexsha} by {commit.author.name} ({commit.message.strip()})')
         # Every commit feeds the cat
-        cat.feed(FOOD_PER_COMMIT)
-        # Every line of code makes the cat more tired
-        line_changes = commit.stats.total['lines']
-        cat.excite(line_changes * EXCITEMENT_PER_LINE)
-        # TODO exhaustion, based on what metric?
+        cat.feed(1)
+        # Every file touched makes the cat more tired
+        cat.exhaust(len(commit.stats.files.keys()))
+        # Every line added to the repository make the cat more excited
+        cat.excite(commit.stats.total['insertions'])
+    print('Done!')
 
     cat.last_update = datetime.now(timezone.utc)
     cat.save()
     _commit_changes(f'Gittycat | Updated my needs', f'{cat.name} (via Gittycat) <gittycat@example.com>')
+
+    # TODO output cat status to console using descriptive text and ascii art
 
 
 @arg('name', type=str, help='Name of your new cat')
